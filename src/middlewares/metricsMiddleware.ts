6e4fe77e591be81f0f22675ed40585ac8760a7bf
Enbiya Göral <100806254+enbiyagoral@users.noreply.github.com>
2024-12-29 18:07:25 +0300

    added more detailed metrics in app

--- src/middlewares/metricsMiddleware.ts
+++ src/middlewares/metricsMiddleware.ts
@@ -11,10 +11,54 @@ const httpRequestDurationMicroseconds = new client.Histogram({
     buckets: [50, 100, 200, 300, 400, 500, 1000]
 });
 
+const httpRequestCount = new client.Counter({
+    name: 'http_request_count',
+    help: 'Count of HTTP requests',
+    labelNames: ['method', 'route', 'code']
+});
+
+const httpErrorCount = new client.Counter({
+    name: 'http_error_count',
+    help: 'Count of HTTP errors',
+    labelNames: ['method', 'route', 'code']
+});
+
+const activeRequests = new client.Gauge({
+    name: 'active_requests',
+    help: 'Number of active HTTP requests',
+    labelNames: ['method', 'route']
+});
+
+const requestSizeHistogram = new client.Histogram({
+    name: 'http_request_size_bytes',
+    help: 'Size of HTTP requests in bytes',
+    labelNames: ['method', 'route'],
+    buckets: [500, 1000, 5000, 10000, 50000, 100000]
+});
+
+const responseSizeHistogram = new client.Histogram({
+    name: 'http_response_size_bytes',
+    help: 'Size of HTTP responses in bytes',
+    labelNames: ['method', 'route'],
+    buckets: [500, 1000, 5000, 10000, 50000, 100000]
+});
+
 export const metricsMiddleware = (req: Request, res: Response, next: NextFunction): void => {
     const end = httpRequestDurationMicroseconds.startTimer();
+    activeRequests.inc({ method: req.method, route: req.path });
+    const requestSize = parseInt(req.headers['content-length'] || '0', 10);
+    requestSizeHistogram.observe({ method: req.method, route: req.path }, requestSize);
+
     res.on('finish', () => {
-        end({ method: req.method, route: req.path, code: res.statusCode });
+        const labels = { method: req.method, route: req.path, code: res.statusCode };
+        end(labels);
+        httpRequestCount.inc(labels);
+        if (res.statusCode >= 400) {
+            httpErrorCount.inc(labels);
+        }
+        activeRequests.dec({ method: req.method, route: req.path });
+        const responseSize = parseInt(res.getHeader('content-length') as string || '0', 10);
+        responseSizeHistogram.observe({ method: req.method, route: req.path }, responseSize);
     });
     next();
 };
