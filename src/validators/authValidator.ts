f0da50c958ab3d6247a511963627423db8439940
enbiyagoral <menbiyagoral@gmail.com>
2025-01-05 22:13:12 +0300

    modified: authValidator.ts

--- src/validators/authValidator.ts
+++ src/validators/authValidator.ts
@@ -4,8 +4,7 @@ export const registerSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
     username: Joi.string().min(3).max(30).required(),
-    connactNumber: Joi.string().pattern(/^[+]?[0-9]{7,15}$/)
-});
+    connactNumber: Joi.string().pattern(/^\(\d{3}\)\s?\d{3}\s?\d{2}\s?\d{2}$/)});
 
 export const loginSchema = Joi.object({
     email: Joi.string().email().required(),
